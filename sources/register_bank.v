5d6be19e00745562c30be30d7c210dbf75381888
NicoCasas <50183649+NicoCasas@users.noreply.github.com>
2023-07-10 18:55:22 -0300

    Update register_bank.v : No enables

--- sources/register_bank.v
+++ sources/register_bank.v
@@ -8,18 +8,15 @@ module register_bank
   (
   // INPUTS
     // Write reg
-		input   wire  [NB_DATA    -1 :0]  i_w_data  ,
-    input   wire  [NB_ADDRESS -1 :0]  i_w_addr  ,
-    input   wire                      i_w_en    ,
-
+		input   wire  [NB_DATA    -1 :0]  i_dr_data  ,
+		input   wire  [NB_ADDRESS -1 :0]  i_dr_addr  ,
+    
     // Read 1 reg
-    input   wire  [NB_ADDRESS -1 :0]  i_r1_addr  ,
-    input   wire                      i_r1_en    ,
-
+		input   wire  [NB_ADDRESS -1 :0]  i_sr1_addr  ,
+    
     // Read 2 reg
-    input   wire  [NB_ADDRESS -1 :0]  i_r2_addr  ,
-    input   wire                      i_r2_en    ,
-
+		input   wire  [NB_ADDRESS -1 :0]  i_sr2_addr  ,
+    
     // Clk
     input                             i_clk      ,
 
@@ -28,41 +25,23 @@ module register_bank
 
   // OUTPUTS
     // Read 1 reg
-		output  wire  [NB_DATA		-1 :0]  o_r1_data  ,
+		output  wire  [NB_DATA		-1 :0]  o_sr1_data  ,
 
     // Read 2 reg
-		output  wire  [NB_DATA		-1 :0]  o_r2_data
+		output  wire  [NB_DATA		-1 :0]  o_sr2_data
   )
   
   // INTERNAL REGS
 	reg [NB_DATA-1:0]   registers [N_REGISTERS-1:0];
-	reg	[NB_DATA-1:0]		reg_o_r1_data;
-	reg	[NB_DATA-1:0]		reg_o_r2_data;
 	
   // WRITE REG LOGIC
   always @(posedge i_clk) begin
-		if(i_w_en) begin
-			registers[i_w_addr] <= i_w_data;
-		end
+		registers[i_dr_addr] <= i_dr_data;
 	end
   
-	// READ 1 REG LOGIC
-	always @(*) begin
-		if(i_r1_en) begin
-			reg_o_r1_data = registers[i_r1_addr];
-		end
-	end
-	
-  // READ 2 REG LOGIC
-	always @(*) begin
-		if(i_r2_en) begin
-			reg_o_r2_data = registers[i_r2_addr];
-		end
-	end
-
   // OUTPUT ASSIGN
-	assign o_r1_data = reg_o_r1_data; // Considerar hacer toda la logica en sentencia usando '?' (if de los wires)
-	assign o_r2_data = reg_o_r2_data;
-
+	assign o_r1_data = registers[i_sr1_addr]; // Considerar hacer toda la logica en sentencia usando '?' (if de los wires)
+	assign o_r2_data = registers[i_sr2_addr];
+	
 endmodule
 	
