99285778db7b241f6bc593741fc8f373bb1ec5a9
NicoCasas <50183649+NicoCasas@users.noreply.github.com>
2023-07-11 21:17:12 -0300

    Update memory.v: Creo que 32 bits direccionado a 8 bits

--- sources/memory.v
+++ sources/memory.v
@@ -25,21 +25,65 @@ module memory
     output   wire  [NB_DATA-1 : 0]      o_r_data
   )
 
+	// LOCALPARAMS
+	localparam word_ADDRESSING = 2'b00;
+	localparam half_ADDRESSING = 2'b01;
+	localparam byte_ADDRESSING = 2'b11;
+	
   // INTERNAL REGS
-	reg  [NB_DATA-1:0]  mem [N_ADDRESS-1:0] ;
-  reg  [NB_DATA-1:0]  reg_o_r_data ;
+	reg  [7:0]  mem [N_ADDRESS-1:0] ;
+	reg  [7:0]  reg_o_r_data ;
 
   // WRITE LOGIC  (sync)
   always @(posedge i_clk) begin
     if(i_w_en) begin
-      mem[i_w_addr] <= i_w_data;
-    end
+			case (i_addressing)
+				word_ADDRESSING: begin
+					mem[i_w_addr	  ] <= i_w_data[7:0];
+					mem[i_w_addr + 1] <= i_w_data[15:8];
+					mem[i_w_addr + 2] <= i_w_data[23:16];
+					mem[i_w_addr + 3] <= i_w_data[31:24];
+				end
+				half_ADDRESSING: begin
+					mem[i_w_addr	  ] <= i_w_data[7:0];
+					mem[i_w_addr + 1] <= i_w_data[15:8];
+				end
+				byte_ADDRESSING: begin
+					mem[i_w_addr	  ] <= i_w_data[7:0];
+				end
+				default begin
+					mem[i_w_addr		] <= mem[i_w_add];
+				end
+			endcase
+		end
   end
 
   // READ LOGIC (async)
   always @(*) begin
     if(i_r_en) begin
-      reg_o_r_data = mem[i_r_add];
+			case (i_addressing)
+				
+				2b'00: begin
+					if(i_r_add[1:0]==2b'00) begin
+						reg_o_r_data = {mem[i_r_add+3],mem[i_r_add+2],mem[i_r_add+1],mem[i_r_add]};
+					end
+				end
+				
+				2b'01: begin
+					if(i_r_add[0]==1'b0) begin
+						reg_o_r_data = {mem[i_r_add+3],mem[i_r_add+2],{8{1'b0}},{8{1'b0}}};
+					end
+				end
+				
+				2b'11: begin
+					reg_o_r_data = {mem[i_r_add+3],{8{1'b0}},{8{1'b0}},{8{1'b0}};
+				end
+				
+				default:
+			  	reg_o_r_data = {32,{1'bx}};
+				end
+													
+			endcase
     end
   end
 
