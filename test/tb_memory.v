d64a5e09915b73e0f6b1b721f09507aaba6beb10
NicoCasas <50183649+NicoCasas@users.noreply.github.com>
2023-07-12 19:51:50 -0300

    Update tb_memory.v
    No hay errores de syntaxis, pero hay que ver porque hay algo que está andando mal :))))

--- test/tb_memory.v
+++ test/tb_memory.v
@@ -1,10 +1,12 @@
+// Code your testbench here
+// or browse Examples
 module tb_memory();
 
-  localparam T = 100;
+  localparam T = 10;
   
-  localparam NB_DATA_BUS = 24;
+  localparam NB_DATA_BUS = 32;
   localparam NB_DATA = 8;
-  localparam NB_ADDRESS = 6;
+  localparam NB_ADDRESS = 3;
 
   localparam word_ADDRESSING = 2'b00;
   localparam half_ADDRESSING = 2'b01;
@@ -20,22 +22,23 @@ module tb_memory();
   reg                    r_en;
   reg  [1:0]             r_addressing;
 
-  wire   clk;
+  reg   clk;
 
+  
   memory #() memory_uut(
-    i_r_addr        (r_addr),
-    i_r_en          (r_en),
-    i_r_addressing  (r_addressing),
+    .i_r_addr        (r_addr),
+    .i_r_en          (r_en),
+    .i_r_addressing  (r_addressing),
 
 
-    i_w_addr        (w_addr),
-    i_w_data        (w_data),
-    i_w_en          (w_en),
-    i_w_addressing  (w_addressing),
+    .i_w_addr        (w_addr),
+    .i_w_data        (w_data),
+    .i_w_en          (w_en),
+    .i_w_addressing  (w_addressing),
 
-    i_clk           (clk),
+    .i_clk           (clk),
 
-    o_r_data        (r_data)
+    .o_r_data        (r_data)
 
   );
 
@@ -46,18 +49,22 @@ module tb_memory();
   end
 
   initial begin
+    $dumpfile("dump.vcd"); $dumpvars;
+    clk = 1'b0;
     #10
     w_en = 1'b0;
+	r_en = 1'b0;
     w_addr = 0;
     w_data = 0;
 
-    #90
+
+    #10
     w_data = 0;
     
     #(2*T)
     w_data = 32'h0123abcd;
     w_addressing = 2'b00;
-    w_addr = 6'b000000;
+    w_addr = 3'b000;
     w_en = 1'b1;
 
     #(T)
@@ -65,23 +72,23 @@ module tb_memory();
     
     #(2*T)
     r_addressing = 2'b00;
-    r_addr = 6'b000000;
+    r_addr = 3'b000;
     r_en = 1'b1;
 
-    #(2*T)
+    #(T)
     r_en = 1'b0;
     
-    #(2*T)
+    #(T)
     r_addressing = 2'b11;
-    r_addr = 6'b000000;
+    r_addr = 3'b000;
     r_en = 1'b1;
     
-    #(2*T)
+    #(T)
     r_en = 1'b0;
  
-    #(2*T)
+    #(T)
     r_addressing = 2'b10;
-    r_addr = 6'b000000;
+    r_addr = 3'b000;
     r_en = 1'b1;
 
 
@@ -89,7 +96,5 @@ module tb_memory();
     $finish();
     
   end
-
-
   
 endmodule
