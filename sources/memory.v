76876349b2e8587240966cdbb9df6de0b6f4fd34
NicoCasas <50183649+NicoCasas@users.noreply.github.com>
2023-07-17 17:57:16 -0300

    Update memory.v: "Anda"

--- sources/memory.v
+++ sources/memory.v
@@ -1,8 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 13.07.2023 14:48:00
+// Design Name: 
+// Module Name: memory
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
 // Code your design here
 module memory
   #(
   // PARAMETERS
-    parameter NB_DATA = 32,
+    parameter NB_DATA_BUS = 32,
+    parameter NB_DATA = 8,
     parameter N_ADDRESS = 64,
 	parameter NB_ADDRESS = $clog2(N_ADDRESS) // No me acuerdo si habia que sumar o restar 1
     
@@ -15,17 +38,17 @@ module memory
 	input	 wire	 [ 1 : 0 ]			i_r_addressing,
 
     // Write
-    input    wire  [ NB_DATA-1 : 0 ]    i_w_data,
+    input    wire  [ NB_DATA_BUS-1 : 0 ]    i_w_data,
     input    wire  [ NB_ADDRESS-1:0]    i_w_addr,
     input    wire                       i_w_en  ,
 	input	 wire	 [ 1 : 0 ]			i_w_addressing,
 
     // Clock
-    input    wire  [ NB_DATA-1:0 ]      i_clk,
+    input    wire                       i_clk,
 
   // OUTPUTS
     // Read
-    output   wire  [NB_DATA-1 : 0]      o_r_data
+    output   wire  [NB_DATA_BUS-1 : 0]      o_r_data
   );
 
 	// LOCALPARAMS
@@ -34,8 +57,8 @@ module memory
 	localparam byte_ADDRESSING = 2'b11;
 	
   // INTERNAL REGS
-	reg  [7:0]  mem [N_ADDRESS-1:0] ;
-	reg  [7:0]  reg_o_r_data ;
+	reg  [NB_DATA-1:0]  mem [0:N_ADDRESS-1] ;
+	reg  [NB_DATA_BUS-1:0]  reg_o_r_data ;
 
   // WRITE LOGIC  (sync)
   always @(posedge i_clk) begin
@@ -71,32 +94,38 @@ module memory
   end
 
   // READ LOGIC (async)
-  always @(i_r_en) begin
+  always @(*) begin
     if(i_r_en) begin
-			case (i_r_addressing)
-				
-				word_ADDRESSING: begin
-                  if(i_r_addr[1:0]==2'b00) begin
-						reg_o_r_data = {mem[i_r_addr+3],mem[i_r_addr+2],mem[i_r_addr+1],mem[i_r_addr]};
-					end
-				end
-				
-				half_ADDRESSING: begin
-                  if(i_r_addr[0]==1'b0) begin
-						reg_o_r_data = {{8{1'b0}},{8{1'b0}},mem[i_r_addr+1],mem[i_r_addr]};
-					end
-				end
-				
-				byte_ADDRESSING: begin
-                  reg_o_r_data = {{8{1'b0}},{8{1'b0}},{8{1'b0}},mem[i_r_addr]};
-				end
-				
-				default: begin
-                  reg_o_r_data = {32{1'bx}};
-				end
-													
-			endcase
+        case (i_r_addressing)
+    
+            word_ADDRESSING: begin
+              if(i_r_addr[1:0]==2'b00) begin
+                    reg_o_r_data = {{mem[i_r_addr+3]},{mem[i_r_addr+2]},{mem[i_r_addr+1]},{mem[i_r_addr]}};
+                end
+                else reg_o_r_data = {32{1'bz}};
+            end
+            
+            half_ADDRESSING: begin
+              if(i_r_addr[0]==1'b0) begin
+                    reg_o_r_data = {{16{1'b0}},mem[i_r_addr+1],mem[i_r_addr]};
+                end
+                else reg_o_r_data = {32{1'bz}};
+            end
+            
+            byte_ADDRESSING: begin
+              reg_o_r_data = {{24{1'b0}},mem[i_r_addr]};
+            end
+            
+            default: begin
+              reg_o_r_data = {32{1'bz}};
+            end
+                                                
+        endcase
+        
     end
+    
+    else reg_o_r_data = {32{1'bz}};
+  
   end
 
   // OUTPUT ASSIGN
