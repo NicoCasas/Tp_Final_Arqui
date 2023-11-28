bdad34f9d9cca7b01805affdc5bf2a7e4eb5a883
NicoCasas <50183649+NicoCasas@users.noreply.github.com>
2023-11-28 19:31:29 -0300

    final data_mem

--- src/memory.v
+++ src/memory.v
@@ -23,43 +23,72 @@
 module memory
 #(
 //PARAMETERS
-    parameter NB_DATA       =  32   ,
-    parameter NB_ADDRESS    =  4    ,
-    parameter N_ADDRESS     =  16
+    parameter NB_DATA       =   32  ,
+    parameter N_ADDRESS     =   32  ,
+    parameter INIT_FILE     =  "",// "file_to_test_if.mem"  ,
+    parameter COL_WIDTH     =    8  ,
+    parameter NB_ADDRESS    =  $clog2(N_ADDRESS)    
+
 )(
-//INPUTS
-    //read
-    input   wire    [NB_ADDRESS-1:0]    i_r_addr    ,
-    //input   wire    [NB_DATA-1:0]       i_data_r    ,
-    input   wire                        i_r_en      ,
+    //OUTPUTS    
+    output  wire    [NB_DATA-1:0]       o_r_data    ,
+
+    output  wire    [NB_DATA-1:0]       o_d_r_data  ,
     
-    //write    
-    input   wire    [NB_ADDRESS-1:0]    i_w_addr    ,
+    //INPUTS
+    input   wire                        i_en        ,
+    input   wire                        i_r_en      ,
+    input   wire    [NB_ADDRESS-1:0]    i_addr      ,
     input   wire    [NB_DATA-1:0]       i_w_data    ,
-    input   wire                        i_w_en      ,
+    input   wire    [3:0]               i_w_en      ,
+
+    input   wire                        i_d_en      ,
+    input   wire    [NB_ADDRESS-1:0]    i_d_addr    ,
 
     //clk
-    input   wire                        i_clk       ,
-
-    
-//OUTPUTS
-    //read    
-    output  wire    [NB_DATA-1:0]       o_r_data
+    input   wire                        i_clk       
     
    );
    
-    reg [NB_DATA-1:0]   mem [N_ADDRESS-1:0];
-    reg [NB_DATA-1:0]   reg_o_r_data;
+    reg [NB_DATA-1:0] mem [0:N_ADDRESS-1];
+    //reg [NB_ADDRESS-1:0] addr;
+    reg [NB_DATA-1:0] reg_o_r_data;
+    reg [NB_DATA-1:0] reg_o_d_r_data;
    
-    always @(posedge i_clk) begin
-        if(i_r_en)begin
-            reg_o_r_data <= mem[i_r_addr];
+    always @(negedge i_clk) begin
+        if(i_en) begin
+            if(i_w_en[0])begin
+                mem[i_addr][7:0] <= i_w_data[7:0];
+            end
+            if(i_w_en[1])begin
+                mem[i_addr][15:8] <= i_w_data[15:8];
+            end
+            if(i_w_en[2])begin
+                mem[i_addr][23:16] <= i_w_data[23:16];
+            end
+            if(i_w_en[3])begin
+                mem[i_addr][31:24] <= i_w_data[31:24];
+            end
+            
+            //addr <= i_addr;
+            if(i_r_en) reg_o_r_data <= mem[i_addr];
+            else       reg_o_r_data <= {NB_DATA{1'b0}};
         end
-        if(i_w_en)begin
-            mem[i_w_addr] <= i_w_data;
-        end
+        
     end
    
-    assign o_r_data = reg_o_r_data;  
+   always @(negedge i_clk) begin
+        if(i_d_en) reg_o_d_r_data <= mem[i_d_addr]; 
+   end
+   
+    initial if (INIT_FILE) begin
+        $readmemb(INIT_FILE,mem); 
+    end
+
+   
+   //output assign
+   assign o_r_data = reg_o_r_data;
+   
+   assign o_d_r_data = reg_o_d_r_data;
    
 endmodule
